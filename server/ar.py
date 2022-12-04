e8ba08db5d08d97dd221abd953128bc587cfc2b1
suguuss <spam.sugusgaming@gmail.com>
2022-12-04 02:36:55 +0100

    Kinda working

--- server/ar.py
+++ server/ar.py
@@ -10,20 +10,33 @@ def get_homography_matrix(kp1, kp2, good):
 	matrix, mask = cv2.findHomography(srcPts, dstPts, cv2.RANSAC, 5)
 	return matrix, mask
 
+def stack_images(base_img, img):
+	alpha = np.sum(img, axis=-1) > 0
+	alpha = np.uint8(alpha * 255)
+	img_with_alpha = cv2.merge((img, alpha))
+
+	alpha_calque = img_with_alpha[:, :, 3] / 255.0
+	alpha_autre = 1.0 - alpha_calque
+
+	for c in range(3):
+		base_img[:, :, c] = alpha_calque * img_with_alpha[:, :, c] + alpha_autre * base_img[:, :, c]
+
+
+
 if __name__ == "__main__":
 	if not os.path.exists("front.png"):
 		get_screenshot("front.png")
 	frontTargetImg = cv2.imread("front.png")
 	hF, wF, cF = frontTargetImg.shape
 
-	if not os.path.exists("back.png"):
-		get_screenshot("back.png")
-	backTargetImg = cv2.imread("back.png")
+	# if not os.path.exists("back.png"):
+	# 	get_screenshot("back.png")
+	# backTargetImg = cv2.imread("back.png")
 
 	circuit = cv2.imread("../circuit_draw.png")
 	circuit = cv2.resize(circuit, (wF, hF))
 
-	orb = cv2.ORB_create(2500)
+	orb = cv2.ORB_create(1000)
 	bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
 	# FRONT
@@ -39,30 +52,41 @@ if __name__ == "__main__":
 
 	NB_FEATURES = 250
 
-	# feed = get_feed()
-	feed = get_feed(ip="192.168.59.242")
-	# feed = cv2.VideoCapture(0)
+	feed = get_feed()
+	# feed = get_feed(ip="192.168.59.242")
+	# feed = cv2.VideoCapture("/dev/video4")
+	count = 0
 	running = True
 	while running:
 		_, frame = feed.read()
 		
 		kp2, des2 = orb.detectAndCompute(frame,None)
 	
-		matches = bf.match(desfront,des2)
-		matches = sorted(matches, key = lambda x:x.distance)
-		if (len(matches) < NB_FEATURES):
-			cv2.imshow("og",frame)
-			if cv2.waitKey(1) == ord("q"):
-				running = False
-			continue
-
+		matches_front = bf.match(desfront,des2)
+		matches_front = sorted(matches_front, key = lambda x:x.distance)
+		# if (len(matches_front) < NB_FEATURES):
+		# 	cv2.imshow("og",frame)
+		# 	if cv2.waitKey(1) == ord("q"):
+		# 		running = False
+		# 	continue
 		
-		print(len(matches))
+		# matches_back = bf.match(desback,des2)
+		# matches_back = sorted(matches_back, key = lambda x:x.distance)
+		# if (len(matches_back) < NB_FEATURES):
+		# 	cv2.imshow("og",frame)
+		# 	if cv2.waitKey(1) == ord("q"):
+		# 		running = False
+		# 	continue
+
+
+		# print(f"Front : {len(matches_front)} | Back : {len(matches_back)}")
+		matches = matches_front
+
+		# print(len(matches))
 		srcpts = np.float32([kpfront[p.queryIdx].pt for p in matches[:NB_FEATURES]]).reshape(-1, 1, 2)
 		dstpts = np.float32([kp2[p.trainIdx].pt for p in matches[:NB_FEATURES]]).reshape(-1, 1, 2)
 
-		matrix, mask = cv2.findHomography(srcpts, dstpts, cv2.RANSAC, 10)
-
+		matrix, mask = cv2.findHomography(srcpts, dstpts, cv2.RANSAC, 5)
 
 		pts = np.float32([[0,0], [0, hF], [wF, hF], [wF, 0]]).reshape(-1, 1, 2)
 		dst = cv2.perspectiveTransform(pts, matrix)
@@ -71,15 +95,7 @@ if __name__ == "__main__":
 
 		warped = cv2.warpPerspective(circuit, matrix, (frame.shape[1], frame.shape[0]))
 
-		alpha = np.sum(warped, axis=-1) > 0
-		alpha = np.uint8(alpha * 255)
-		draw_with_alpha = cv2.merge((warped, alpha))
-
-		alpha_calque = draw_with_alpha[:, :, 3] / 255.0
-		alpha_autre = 1.0 - alpha_calque
-
-		for c in range(3):
-			frame[:, :, c] = alpha_calque * draw_with_alpha[:, :, c] + alpha_autre * frame[:, :, c]
+		stack_images(frame, warped)
 
 		# cv2.imshow("image",img3)
 		cv2.imshow("og",frame)
